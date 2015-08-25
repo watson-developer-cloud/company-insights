da9b6e5e274d7af68842629f759e5d5ce9410ea5
Daniel Cheng <dcheng@us.ibm.com>
2015-08-25 18:00:46 -0400

    broken

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var Twitter = require('twitter');
 var express = require('express');
 var watson = require('watson-developer-cloud');
-
+var logger = require('./logger.js');
 var config = require('./config.js');
 
 var app = express()
 
-var client = new Twitter(config.services.twitter);
+var twitterClient = new Twitter(config.services.twitter);
 
 var pi = watson.personality_insights(config.services.personality_insights);
+
+var MAX_COUNT = 200;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -16,7 +18,19 @@ app.get('/', function (req, res) {
  
 app.get('/tweets/:id', function(req, res) {
   var params = {screen_name: req.params.id};
-  client.get('statuses/user_timeline', params, function(error, tweets, response){
+  twitterClient.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(tweets));
+    } else {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(error.stack);
+    }
+  });
+});
+
+app.get('/getalltweets/:id', function(req, res) {
+  getAllTweets(req.params.id, function(error, tweets) {
     if (!error) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end(JSON.stringify(tweets));
@@ -48,3 +62,52 @@ app.get('/pi', function(req, res) {
 
 app.listen(8080);
 
+var englishAndNoRetweet = function(tweet) {
+  return tweet.lang === 'en' && !tweet.retweeted;
+};
+
+function toContentItem(tweet) {
+  return {
+    id: tweet.id_str,
+    userid: tweet.user.id_str,
+    sourceid: 'twitter',
+    language: 'en',
+    contenttype: 'text/plain',
+    content: tweet.text.replace('[^(\\x20-\\x7F)]*',''),
+    created: Date.parse(tweet.created_at)
+  };
+}
+
+function getAllTweets(screen_name, callback, current) {
+  logger.info('getTweets for:', screen_name);
+
+  var tweets = current || [],
+      params = {
+        screen_name: screen_name,
+        count: MAX_COUNT,
+        exclude_replies: true,
+        trim_user:true
+      };
+
+  var processTweets = function(error, _tweets) {
+    console.log(_tweets);
+    // Check if _tweets its an error
+    if (error)
+      return callback(error);
+
+    var items = _tweets
+    .filter(englishAndNoRetweet)
+    .map(toContentItem);
+
+    tweets = tweets.concat(items);
+    logger.info(screen_name,'_tweets.count:',tweets.length);
+    if (_tweets.length > 1) {
+      // TODO: this doesn't work :(
+      params.max_id = _tweets[_tweets.length-1].id - 1;
+      getAllTweets(screen_name, callback, tweets);
+    } else {
+       callback(null, tweets);
+    }
+  };
+  twitterClient.get('statuses/user_timeline', params, processTweets);
+}
