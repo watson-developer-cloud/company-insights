c1022c3f5c55c779eeeaa139f2d4e4fc47d7ea83
Daniel Cheng <dcheng@us.ibm.com>
2015-08-25 17:03:06 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var Twitter = require('twitter');
+var express = require('express');
+var watson = require('watson-developer-cloud');
+
+var config = require('./config.js');
+
+var app = express()
+
+var client = new Twitter(config.services.twitter);
+
+var pi = watson.personality_insights(config.services.personality_insights);
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+ 
+app.get('/tweets', function(req, res) {
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(tweets));
+    } else {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(error.stack);
+    }
+  });
+});
+
+app.get('/pi', function(req, res) {
+  pi.profile(
+    {
+      text: 'We observe today not a victory of party but a celebration of freedom--symbolizing an end as well as a beginning--signifying renewal as well as change. For I have sworn before you and Almighty God the same solemn oath our forbears prescribed nearly a century and three-quarters ago. The world is very different now. For man holds in his mortal hands the power to abolish all forms of human poverty and all forms of human life. And yet the same revolutionary beliefs for which our forebears fought are still at issue around the globe--the belief that the rights of man come not from the generosity of the state but from the hand of God.',
+      language: 'en'
+    },
+    function (err, response) {
+      if (err) {
+        console.log('error:', err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end(err.stack);
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(JSON.stringify(response));
+      }
+    }
+  );
+});
+
+app.listen(8080);
+
---
+++ config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var logger = require('./logger'),
+  bluemix  = require('./bluemix'),
+  extend   = require('extend'),
+  env      = process.env.VCAP_SERVICES ? 'prod' : 'dev';
+
+var services = {
+  mongodb: 'mongodb://localhost/celebs',
+
+  personality_insights: {
+    url:      '<url>',
+    username: '<username>',
+    password: '<password>',
+    version: 'v2'
+  },
+
+  twitter: [
+  // Twitter app credentials: https://apps.twitter.com/app
+  {
+    consumer_key:       '<consumer_key>',
+    consumer_secret:    '<consumer_secret>',
+    access_token_key:   '<access_token_key>',
+    access_token_secret:'<access_token_secret>'
+  }]
+};
+
+
+// Get the service
+if (env === 'prod') {
+  services.mongodb = bluemix.serviceStartsWith('mongodb').url;
+  services.personality_insights = extend({'version':'v2'}, bluemix.serviceStartsWith('personality_insights'));
+}
+
+logger.info('mongodb:',services.mongodb);
+logger.info('personality_insights:',services.personality_insights);
+
+module.exports = {
+    services: services,
+    host: '127.0.0.1',
+    port: 3000
+};
