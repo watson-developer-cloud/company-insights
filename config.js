ee099842baaf238f80a473987aeae2b2365711f0
Nathan Friedly <nathan@nfriedly.com>
2015-08-26 12:39:51 -0400

    adding .env support so that I don't have to avoid checking in config.js

--- config.js
+++ config.js
@@ -1,29 +1,31 @@
 'use strict';
 
+require('dotenv').load({silent: true});
+
 var bluemix  = require('./bluemix'),
    extend   = require('extend');
 
 var services = {
 
  // http://www.alchemyapi.com/api/register.html
- alchemy_api_key: '<api_key>',
+ alchemy_api_key: process.env.ALCHEMY_AIP_KEY || '<api_key>',
 
  // PI credentials are automatically loaded for bluemix apps with a bound personality insights service
  // For local testing, get credentials by creating a bluemix app, adding a PI service, and clicking Show Credentials
  // https://console.ng.bluemix.net/
  personality_insights: {
-   url:      '<url>',
-   username: '<username>',
-   password: '<password>',
-   version: 'v2'
+   url:      process.env.PERSONALITY_INSIGHTS_URL || 'https://gateway.watsonplatform.net/personality-insights/api',
+   username: process.env.PERSONALITY_INSIGHTS_USERNAME || '<username>',
+   password: process.env.PERSONALITY_INSIGHTS_PASSWORD || '<password>',
+   version: process.env.PERSONALITY_INSIGHTS_VERSION || 'v2'
  },
 
  // Twitter app credentials: https://apps.twitter.com/app
  twitter: {
-   consumer_key:       '<consumer_key>',
-   consumer_secret:    '<consumer_secret>',
-   access_token_key:   '<access_token_key>',
-   access_token_secret:'<access_token_secret>'
+   consumer_key:       process.env.TWITTER_CONSUMER_KEY || '<consumer_key>',
+   consumer_secret:    process.env.TWITTER_CONSUMER_SECRET || '<consumer_secret>',
+   access_token_key:   process.env.TWITTER_ACCESS_TOKEN_KEY || '<access_token_key>',
+   access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET || '<access_token_secret>'
  }
 };
 
