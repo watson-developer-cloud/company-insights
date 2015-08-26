4ae0513fa78f0305a634d82dcda3b0744d450c3d
Nathan Friedly <nathan@nfriedly.com>
2015-08-26 12:03:28 -0400

    moved watson code to it's own file, mocked out news API

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 var express = require('express');
-var watson = require('watson-developer-cloud');
 var logger = require('./logger.js');
 var config = require('./config.js');
 
@@ -8,8 +7,7 @@ var app = express();
 
 var twitterClient = require('./twitter.js');
 
-var personality_insights = watson.personality_insights(config.services.personality_insights);
-
+var watson = require('./watson.js');
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -55,89 +53,29 @@ app.get('/personality_insights/:id', function(req, res) {
     var tweetText = tweets.map(function(tweet) {
       return tweet.content;
     }).join(' ');
-    // personality insights with the concatenated text
-    personality_insights.profile(
-      {
-        text: tweetText,
-        language: 'en'
-      },
-      function (err, response) {
-        if (!err) {
-          res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-          res.end(JSON.stringify(big5(response), null, 2)); // todo: make this safe incase the format doesn't match expectations
-        } else {
-          console.log('error:', err);
-          res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-          res.end(err.stack);
-        }
+
+    // then call PI on the concatenated tweets
+    watson.getBig5PersonalityTraits(tweetText, function (err, big5) {
+      if (err) {
+        console.log('error:', err);
+        res.status(500).send(err.message || err);
       }
-    );
+      res.json(big5);
+    });
+  });
+});
+
+app.get('/news/:id', function(req, res) {
+  watson.getNewsAbout(id, function(err, news) {
+    if (!err) {
+      console.log('error:', err);
+      res.status(500).send(err.message || err);
+    }
+    res.json(news);
   });
 });
 
 app.listen(config.port);
 
-var englishAndNoRetweet = function(tweet) {
-  return tweet.lang === 'en' && !tweet.retweeted;
-};
-
-function toContentItem(tweet) {
-  return {
-    id: tweet.id_str,
-    userid: tweet.user.id_str,
-    sourceid: 'twitter',
-    language: 'en',
-    contenttype: 'text/plain',
-    content: tweet.text.replace('[^(\\x20-\\x7F)]*',''),
-    created: Date.parse(tweet.created_at)
-  };
-}
-
-function getAllTweets(screen_name, callback, previousParams, current) {
-  logger.info('getTweets for:', screen_name);
-
-  var tweets = current || [],
-      params = previousParams || {
-        screen_name: screen_name,
-        count: MAX_COUNT,
-        exclude_replies: true,
-        trim_user:true
-      };
-
-  var processTweets = function(error, _tweets) {
-    // Check if _tweets its an error
-    if (error)
-      return callback(error);
-
-    var items = _tweets
-    .filter(englishAndNoRetweet)
-    .map(toContentItem);
-
-    tweets = tweets.concat(items);
-    logger.info(screen_name,'_tweets.count:',tweets.length);
-    if (_tweets.length > 1) {
-      params.max_id = _tweets[_tweets.length-1].id - 1;
-      getAllTweets(screen_name, callback, params, tweets);
-    } else {
-      // return tweets in order they were posted
-      tweets.reverse();
-      callback(null, tweets);
-    }
-  };
-
-  twitterClient.get('statuses/user_timeline', params, processTweets);
-}
 
 
-
-/**
- * Return the Big 5 Traits normalized
- * @return Array      The 5 main traits
- */
-var big5 = function(tree) {
-  var profile = typeof (tree) === 'string' ? JSON.parse(tree) : tree;
-  var _big5 = profile.tree.children[0].children[0].children;
-  return _big5.map(function(trait) {
-    return { name: trait.name, value: trait.percentage };
-  });
-};
---
+++ watson.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var watson = require('watson-developer-cloud');
+var logger = require('./logger.js');
+var config = require('./config.js');
+var personality_insights = watson.personality_insights(config.services.personality_insights);
+
+
+// https://access.alchemyapi.com/
+
+
+function getBig5PersonalityTraits(text, callback) {
+    personality_insights.profile({text: text, language: 'en'}, function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, extractBig5(response));
+    });
+}
+
+
+/**
+ * Return the Big 5 Traits normalized
+ * @return Array      The 5 main traits
+ */
+var extractBig5 = function(tree) {
+    var profile = typeof (tree) === 'string' ? JSON.parse(tree) : tree;
+    var _big5 = profile.tree.children[0].children[0].children;
+    return _big5.map(function(trait) {
+        return { name: trait.name, value: trait.percentage };
+    });
+};
+
+
+function getNewsAbout(id, callback) {
+    callback(new Error('not implemented'));
+}
+
+
+module.exports.getBig5PersonalityTraits = getBig5PersonalityTraits;
+module.exports.getNewsAbout = getNewsAbout;
