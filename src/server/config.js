ad25feeb02a96c415346d43a309ccb61d37ef785
Nathan Friedly <nathan@nfriedly.com>
2015-08-28 10:48:04 -0400

    adding default url for alchemy news

--- src/server/config.js
+++ src/server/config.js
@@ -6,27 +6,27 @@ var extend = require('extend');
 
 var services = {
 
- // http://www.alchemyapi.com/api/register.html
- alchemy_api_key: process.env.ALCHEMY_AIP_KEY || '<api_key>',
- alchemy_news_url: process.env.ALCHEMY_NEWS_URL || '<alchemy_news_url>',
+  // http://www.alchemyapi.com/api/register.html
+  alchemy_news_url: process.env.ALCHEMY_NEWS_URL || 'https://access.alchemyapi.com/calls/data/GetNews',
+  alchemy_api_key: process.env.ALCHEMY_AIP_KEY || '<api_key>',
 
- // PI credentials are automatically loaded for bluemix apps with a bound personality insights service
- // For local testing, get credentials by creating a bluemix app, adding a PI service, and clicking Show Credentials
- // https://console.ng.bluemix.net/
- personality_insights: {
-   url:      process.env.PERSONALITY_INSIGHTS_URL || 'https://gateway.watsonplatform.net/personality-insights/api',
-   username: process.env.PERSONALITY_INSIGHTS_USERNAME || '<username>',
-   password: process.env.PERSONALITY_INSIGHTS_PASSWORD || '<password>',
-   version: process.env.PERSONALITY_INSIGHTS_VERSION || 'v2'
- },
+  // PI credentials are automatically loaded for bluemix apps with a bound personality insights service
+  // For local testing, get credentials by creating a bluemix app, adding a PI service, and clicking Show Credentials
+  // https://console.ng.bluemix.net/
+  personality_insights: {
+    url:      process.env.PERSONALITY_INSIGHTS_URL || 'https://gateway.watsonplatform.net/personality-insights/api',
+    username: process.env.PERSONALITY_INSIGHTS_USERNAME || '<username>',
+    password: process.env.PERSONALITY_INSIGHTS_PASSWORD || '<password>',
+    version: process.env.PERSONALITY_INSIGHTS_VERSION || 'v2'
+  },
 
- // Twitter app credentials: https://apps.twitter.com/app
- twitter: {
-   consumer_key:       process.env.TWITTER_CONSUMER_KEY || '<consumer_key>',
-   consumer_secret:    process.env.TWITTER_CONSUMER_SECRET || '<consumer_secret>',
-   access_token_key:   process.env.TWITTER_ACCESS_TOKEN_KEY || '<access_token_key>',
-   access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET || '<access_token_secret>'
- }
+  // Twitter app credentials: https://apps.twitter.com/app
+  twitter: {
+    consumer_key:       process.env.TWITTER_CONSUMER_KEY || '<consumer_key>',
+    consumer_secret:    process.env.TWITTER_CONSUMER_SECRET || '<consumer_secret>',
+    access_token_key:   process.env.TWITTER_ACCESS_TOKEN_KEY || '<access_token_key>',
+    access_token_secret:process.env.TWITTER_ACCESS_TOKEN_SECRET || '<access_token_secret>'
+  }
 };
 
 function getBluemixServiceConfig(name) {
