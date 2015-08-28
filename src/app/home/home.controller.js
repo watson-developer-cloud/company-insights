21beb2a494baee2b51277185a1d11fa75deb8a92
Sam Frons <samfrons@gmail.com>
2015-08-27 22:35:02 -0400

    closed issues, compare, favicon, multiple handles, labels, pending multiple bars

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -13,6 +13,13 @@
     this.mainCompany = null;
     this.chartData = [];
 
+    this.companyToCompare = null;
+    this.companiesToCompare = [];
+
+    this.existCompany = false;
+    this.invalidCompany = false;
+    this.limitExceeded = false;
+
     this.analyze = function(){
       personalityInsight
         .analyze(this.mainCompany)
@@ -21,5 +28,39 @@
         });
     };
 
+    this.addCompanyToCompare = function(){
+      if (this.companiesToCompare.length == 7) {
+        this.limitExceeded = true;
+      } 
+      else if (this.companyToCompare == null || this.companyToCompare == "") {
+        this.invalidCompany = true;
+      }
+      else if (this.companiesToCompare.indexOf(this.companyToCompare) > -1) {
+        this.existCompany = true;
+      } 
+      else{
+        this.companiesToCompare.push(this.companyToCompare);
+        this.companyToCompare = null;
+      };
+      
+    };
+
+    this.validateCompany = function(){
+
+      this.existCompany = false;
+      this.invalidCompany = false;
+      this.limitExceeded = false;
+
+      if (this.companiesToCompare.length == 7) {
+        this.limitExceeded = true;
+      } 
+      else if (this.companyToCompare == null || this.companyToCompare == "") {
+        this.invalidCompany = true;
+      }
+      else if (this.companiesToCompare.indexOf(this.companyToCompare) > -1) {
+        this.existCompany = true;
+      };
+      
+    };
   }
 })();
