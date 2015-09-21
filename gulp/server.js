a9a28e8f237c14a5ba64006873cdc4348da99cf8
Nathan Friedly <nathan@nfriedly.com>
2015-09-21 15:03:53 -0400

    fixing proxy middleware for development

--- gulp/server.js
+++ gulp/server.js
@@ -33,7 +33,7 @@ function browserSyncInit(baseDir, browser) {
    *
    * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.0.5/README.md
    */
-  // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', proxyHost: 'jsonplaceholder.typicode.com'});
+  server.middleware = proxyMiddleware('/api', {target: 'http://localhost:8080/'});
 
   browserSync.instance = browserSync.init({
     startPath: '/',
